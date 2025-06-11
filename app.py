90e044f5ac1d24fe84825e34c16ca838667cc435
Drebak666 <respino85@gmail.com>
2025-06-11 23:09:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ def tareas_por_mes(mes_str):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
+
